a9046fb41b5652c77a7bccf09a0cb6eaa16596a5
Sarah Jade <contact@sarahjadegreen.com>
2018-03-05 14:24:59 +0800

    things happened

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,2 +1,22 @@
-console.log('Yes');
-$(document).ready();
+/*
+** Function that runs when the website has been loaded
+*/
+function init() {
+
+  // Log
+  console.log('Yoo!');
+
+  // Scroll reveal
+      window.sr = ScrollReveal({
+        reset: false,
+        distance: '40px',
+        scale: 1,
+        viewFactor: 0.5,
+        duration: 1500
+      });
+      sr.reveal('.fade-in-element');
+
+}
+
+// When document is ready run the function Init
+$(document).ready(init);
